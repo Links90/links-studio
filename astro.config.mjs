309e7da6e9d366185909c46dca4d3e5c3ad15c2d
Links90 <luke.inks@hotmail.com>
2026-06-26 17:37:46 +0200

    Use Webflow Cloud Astro mount configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,11 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  base: "/studio",
+  base: "CLOUD_MOUNT_PATH",
   output: "server",
-
-  build: {
-    assetsPrefix: "/studio",
-  },
-
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
-    imageService: "passthrough",
   }),
-
-  trailingSlash: "never",
 });
